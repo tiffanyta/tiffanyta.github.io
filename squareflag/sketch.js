eba0a42ed57626fd75877bc133b5dcd1bf052338
tiffanyta <hello@tiffanyta.com>
2015-10-29 00:25:18 -0700

    squareflag 2
    included mouse pressed for mobile

--- squareflag/sketch.js
+++ squareflag/sketch.js
@@ -31,7 +31,7 @@ function draw() {
   var x = 0;
   while (x < 30) {
     var y = 0;
-    while (y < 30) { 
+    while (y < 60) { 
       // we give a unique rotation amount to each rectangle, depending
       // on which column and row the rectangle is located (x and y)
       draw_rotating_rectangle(50 + x * 40, 50 + y * 30, 16, rot + x + y);
@@ -42,5 +42,26 @@ function draw() {
   rot = rot + map(mouseX, 0, width, 0.2, 0.8);
 }
 
+function mousePressed(){
+  rect(0,0,width,height);
+    red=255.0/width*mouseX;
+    green=255.0/height*mouseY;
+    blue=255.0/width+height*mouseY+mouseY;
+
+  background(red, green, blue);
+
+  var x = 0;
+  while (x < 30) {
+    var y = 0;
+    while (y < 30) { 
+      // we give a unique rotation amount to each rectangle, depending
+      // on which column and row the rectangle is located (x and y)
+      draw_rotating_rectangle(50 + x * 40, 50 + y * 30, 16, rot + x + y);
+      y = y + 1;
+    }
+    x = x + 1;
+  }
+  rot = rot + map(mouseX, 0, width, 0.2, 0.8);
+}
 
 
